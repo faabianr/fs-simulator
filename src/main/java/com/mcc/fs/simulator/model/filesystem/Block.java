73d504400a65980e7d9a1e4d050dc8354b969900
Fabian Rivera <fabian.riveram@gmail.com>
2021-04-20 21:45:41 -0500

    Fix builder issue

--- src/main/java/com/mcc/fs/simulator/model/filesystem/Block.java
+++ src/main/java/com/mcc/fs/simulator/model/filesystem/Block.java
@@ -1,14 +1,12 @@
 package com.mcc.fs.simulator.model.filesystem;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Data
 @Slf4j
 public abstract class Block {
