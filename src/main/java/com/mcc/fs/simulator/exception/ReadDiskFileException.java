6a1562ee58e7b990f62cfbc81f98b09f44e9d8fc
Fabian Rivera <fabian.riveram@gmail.com>
2021-04-28 22:59:36 -0500

    Implement listdir with args

---
+++ src/main/java/com/mcc/fs/simulator/exception/ReadDiskFileException.java
@@ -0,0 +1,9 @@
+package com.mcc.fs.simulator.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Unable to read from disk file")
+public class ReadDiskFileException extends RuntimeException {
+
+}
